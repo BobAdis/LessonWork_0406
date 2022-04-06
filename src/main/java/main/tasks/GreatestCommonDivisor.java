d4ff40896b9d212b4d4727c9ea60c830fc85fa81
BobAdis <romhanyi.botond@gmail.com>
2022-04-06 18:13:38 +0200

    The Gratest

--- src/main/java/main/tasks/GreatestCommonDivisor.java
+++ src/main/java/main/tasks/GreatestCommonDivisor.java
@@ -3,10 +3,15 @@ package main.tasks;
 public class GreatestCommonDivisor {
 
     public static void main(String[] args) {
-        int n1 = 0, n2 = 0;
 
-        int smaller = (n1 < n2 ? n1 : n2);
-        int bigger = (n1 > n2 ? n1 : n2);
+    }
+
+    /**
+     * Megtalálja két nemnegatív egész szám legnagyobb közös osztóját.
+     */
+    public static int findGCD(int number1, int number2) {
+        int smaller = (number1 < number2 ? number1 : number2);
+        int bigger = (number1 > number2 ? number1 : number2);
 
         while (smaller != bigger) {
             int sub = bigger - smaller;
@@ -14,16 +19,6 @@ public class GreatestCommonDivisor {
             bigger = (smaller > sub ? smaller : sub);
             smaller = (smaller < sub ? smaller : sub);
         }
-
-        System.out.println(smaller);
+        return smaller;
     }
-
-    /**
-     * Megtalálja két nemnegatív egész szám legnagyobb közös osztóját.
-     */
-    public static int findGCD(int number1, int number2) {
-        // write your code here
-        return -1;
-    }
-
 }
